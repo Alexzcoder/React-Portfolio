705d1c4a1a0a785779d58715924b8c6e25a05dbe
Alex <alexzbest123@gmail.com>
2019-07-02 10:19:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rodideal alexandru 
+
+
+*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Rodideal Alexandru Portfolio</h1>
+        <div>
+        
+        </div>
+ 
+     
+      </div>
+    );
+  }
+}
